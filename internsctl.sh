cc99ae790b9743f0fed0ef69bf2b5d2f3ddff01b
sahil10023 <81016048+sahil10023@users.noreply.github.com>
2023-11-08 16:34:26 +0530

    Update internsctl.sh

--- internsctl.sh
+++ internsctl.sh
@@ -3,44 +3,36 @@
 
 VERSION="1.0.1"
 
-function internsctl-cpu {
-  # Add your CPU information retrieval logic here
-  lscpu
-}
-
-function create_user {
-  if [ -z "$2" ]; then
-    echo "Usage: $0 user create <username>"
-    exit 1
-  fi
-
-  username="$2"
-
-  # Add your user creation logic here
-  # Example: sudo adduser "$username"
-  echo "User $username created successfully"
-}
-
-function list_users {
-  # Add your user listing logic here
-  # Example: getent passwd
-  echo "List of users:"
-  getent passwd | cut -d: -f1
-}
-
 function file_info {
   if [ -z "$2" ]; then
-    echo "Usage: $0 file getinfo <file name>"
+    echo "Usage: $0 file getinfo [options] <file name>"
     exit 1
   fi
 
   filename="$2"
 
   if [ -e "$filename" ]; then
-    echo "file :- $filename"
-    echo "Access :- $(stat -c %a "$filename")"
-    echo "Size :- $(stat -c %s "$filename") bytes"
-    echo "Owner :- $(stat -c %U "$filename")"
+    while [[ $# -gt 0 ]]; do
+      case "$1" in
+        --size | -s)
+          echo "Size: $(stat -c %s "$filename") bytes"
+          ;;
+        --permissions | -p)
+          echo "Permissions: $(stat -c %a "$filename")"
+          ;;
+        --owner | -o)
+          echo "Owner: $(stat -c %U "$filename")"
+          ;;
+        --last-modified | -m)
+          echo "Last Modified: $(stat -c %y "$filename")"
+          ;;
+        *)
+          echo "Unknown option: $1"
+          exit 1
+          ;;
+      esac
+      shift
+    done
   else
     echo "File not found: $filename"
     exit 1
@@ -53,37 +45,11 @@ function show_version {
 
 # Check for the subcommand or options and execute the corresponding function
 case "$1" in
-  "cpu")
-    shift
-    case "$1" in
-      "getinfo")
-        internsctl-cpu
-        ;;
-      *)
-        echo "Unknown cpu subcommand: $1"
-        exit 1
-        ;;
-    esac
-    ;;
-  "user")
-    shift
-    case "$1" in
-      "create")
-        create_user "$@"
-        ;;
-      "list")
-        list_users
-        ;;
-      *)
-        echo "Unknown user command: $1"
-        exit 1
-        ;;
-    esac
-    ;;
   "file")
     shift
     case "$1" in
       "getinfo")
+        shift
         file_info "$@"
         ;;
       *)
@@ -96,7 +62,7 @@ case "$1" in
     show_version
     ;;
   *)
-    echo "Usage: $0 {cpu|user|file|--version}"
+    echo "Usage: $0 {file|--version}"
     exit 1
     ;;
 esac
